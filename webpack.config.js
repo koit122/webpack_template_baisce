79c3d47f8982bc6661d78cbaa3a9bdc9204c78b6
koit <shj123612@gmail.com>
2023-06-01 09:18:39 +0900

    test5

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
-const path = require('path');
-const HtmlPlugin = require('html-webpack-plugin');
+const path = require('path')
+const HtmlPlugin = require('html-webpack-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
 module.exports={
   entry:'./src/main.js',
@@ -20,7 +20,8 @@ module.exports={
         ]
       },
       {
-        test:/.js$/,
+        test:/\.js$/,
+        exclude:/node_modules/, //제외할 경로
         use:[
           'babel-loader'
         ]
@@ -39,6 +40,8 @@ module.exports={
     })
   ],
   devServer:{
-    host:'localhost'
+    host:'localhost',
+    port:8080,
+    hot:true
   }
 }
